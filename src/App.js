80a6ca5a866a9041ec1c4cadb3b6db6f8fcb05eb
Pranjal Verma <verma.pranjalraj@gmail.com>
2022-07-18 14:32:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let name ="Pranjal Verma";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <section>Hello {name}</section>
+    </div>
+  );
+}
+
+export default App;

0ad5a9b49f145f93c372afb267994bb750482013
Pranjal Verma <verma.pranjalraj@gmail.com>
2022-08-19 20:06:01 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 import './App.scss';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
 import Counter from './components/Counter';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App container mt-5">
-      {/* <div className="name">App</div> */}
-     <Home name="Pranjal Verma" course="ReactJs" timeline={4}/>
-     <Counter/>
-    </div>
+    // <>
+    //   <Navbar />
+    //   <div className="App container mt-5">
+    //     <Home name="Pranjal Verma" course="ReactJs" timeline={2} />
+    //     <Counter />
+    //   </div>
+    // </>
+<Router>
+<Navbar />
+    <Routes>
+      <Route path="/counter" element = {<Counter />}>
+      </Route>
+      <Route path="/" element = { <Home name="Pranjal Verma" course="ReactJs" timeline={2} />}>
+     
+      </Route>
+    </Routes>
+    </Router>
   );
 }
 

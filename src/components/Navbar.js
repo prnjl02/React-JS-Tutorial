51a9108488626313e2ecc94ff5b0fda915a66586
Pranjal Verma <verma.pranjalraj@gmail.com>
2022-08-19 20:09:47 +0530

    navbar theme change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                {/* <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button> */}
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item active">
@@ -23,9 +19,4 @@ export default function Navbar() {
         </div>
     )
 }
-
-
-
-
-
 
